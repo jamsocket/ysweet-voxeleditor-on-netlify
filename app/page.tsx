a989617257b14d02552983e4b7088f2c8854cca7
Felicia <ffeliciachang@gmail.com>
2024-09-25 16:22:01 -0700

    env var usage

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,13 @@
 import { VoxelEditor } from "./VoxelEditor";
 import { YDocProvider } from "@y-sweet/react";
 import { getOrCreateDocAndToken } from "@y-sweet/sdk";
+import { CONNECTION_STRING } from "./lib/config";
 
 type HomeProps = {
   // searchParams is an object provided by Next.js containing URL parameters.
   // See: https://nextjs.org/docs/app/api-reference/file-conventions/page
   searchParams: Record<string, string>;
 };
-
-// ****************************************************************
-// ** TO DO: Replace this with your quickstart connection string **
-// ****************************************************************
-// For simplicity, we are hard-coding the connection string in the
-// file. In a real app, you should instead pass this in through a
-// secret store or environment variable.
-const CONNECTION_STRING =
-  process.env.Y_SWEET_CONNECTION_STRING ?? ""
 
 export default async function Home({ searchParams }: HomeProps) {
   const clientToken = await getOrCreateDocAndToken(
