bbff69d19e914d44ef93715a0e5242bba11bbd17
Taylor Baldwin <taylorbaldwin@gmail.com>
2024-10-02 12:17:50 -0400

    changes per comments

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 import { VoxelEditor } from "./VoxelEditor";
 import { YDocProvider } from "@y-sweet/react";
 
-const randomDocId = () => Math.random().toString(36).substring(2, 10);
+function randomDocId() {
+  return Math.random().toString(36).substring(2, 10);
+}
 
 export default async function Home(props: { searchParams: { doc: string } }) {
   const docId = props.searchParams.doc || randomDocId();
